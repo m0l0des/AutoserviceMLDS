e3894d474de257d6ac55bea1b367369f23ce0aae
mixoh00@gmail.com <mixoh00@gmail.com>
2021-02-26 15:07:37 +0300

    yo!

---
+++ AutoserviceMLDS/Core.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoserviceMLDS
+{
+    class Core
+    {
+        public static mloginovEntities1 DB = new mloginovEntities1();
+
+    }
+
+}
